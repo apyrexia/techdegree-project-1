86cb32dec577626b8f034190a9c90024e5590238
Ethan W <elwaterson@gmail.com>
2020-03-18 19:37:51 -0400

    Initial commit

---
+++ quotes.js
@@ -0,0 +1,85 @@
+let quotes = [
+  {
+    quote: `To be whole is to be part; true voyage is return.`,
+    source: `Ursula K. Le Guin`,
+    citation: `The Dispossessed`,
+    year: `1974`
+  },
+  {
+    quote: `If you can see a thing whole,” he said, “it seems that it’s always beautiful. Planets, lives.... But close up, a world’s all dirt and rocks. And day to day, life’s a hard job, you get tired, you lose the pattern. You need distance, interval. The way to see how beautiful the earth is, is to see it as the moon. The way to see how beautiful life is, is from the vantage point of death.`,
+    source: `Ursula K. Le Guin`,
+    citation: `The Dispossessed`,
+    year: `1974`
+  },
+  {
+    quote: `I thought once that gods are the opposite of death, but I see now they are more dead than anything, for they are unchanging, and can hold nothing in their hands.`,
+    source: `Madeline Miller`,
+    citation: `Circe`,
+    year: `2018`
+  },
+  {
+    quote: `Anyone who holds a true opinion without understanding is like a blind man on the right road.`,
+    source: `Words of Socrates as quoted by Plato`,
+    citation: `Republic`,
+    year: `375 BC`
+  },
+  {
+    quote: `It is a disgrace to grow old through sheer carelessness before seeing what manner of man you may become by developing your bodily strength and beauty to their highest limit. But you cannot see that, if you are careless; for it will not come of its own accord.`,
+    source: `Words of Socrates as quoted by Xenophon`,
+    citation: `Memorabilia`,
+    year: `~ 371 BC`
+  },
+  {
+    quote: `Wonder is the feeling of a philosopher, and philosophy begins in wonder.`,
+    source: `Words of Socrates as quoted by Plato`,
+    citation: `Theaetetus`,
+    year: `~ 369 BC`
+  },
+  {
+    quote: `It is a strange fate that we should suffer so much fear and doubt over so small a thing… such a little thing. -Boromir`,
+    source: `J.R.R. Tolkien`,
+    citation: `The Fellowship of the Ring`,
+    year: `1954`
+  },
+  {
+    quote: `The world is indeed full of peril, and in it there are many dark places; but still there is much that is fair, and though in all lands love is now mingled with grief, it grows perhaps the greater. -Haraldir`,
+    source: `J.R.R. Tolkien`,
+    citation: `The Fellowship of the Ring`,
+    year: `1954`
+  },
+  {
+    quote: `Time does not tarry ever,' he said; 'but change and growth is not in all things and places alike. For the Elves the world moves, and it moves both very swift and very slow. Swift, because they themselves change little, and all else fleets by: it is a grief to them. Slow, because they do not count the running years, not for themselves. Yet beneath the Sun all things must wear to an end at last. -Legolas`,
+    source: `J.R.R. Tolkien`,
+    citation: `The Fellowship of the Ring`,
+    year: `1954`
+  },
+  {
+    quote: `Every lie we tell incurs a debt to the truth, sooner or later that debt is paid.`,
+    source: `Valery Legasov`,
+    citation: `From the Serial "Chernobyl"`,
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+];
