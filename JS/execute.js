a8dfa1d1b8607c2d57b80117a9e2d0cf647d7959
Ethan W <elwaterson@gmail.com>
2020-03-19 13:58:52 -0400

    Created a timer to show a new quote every 25 seconds

--- JS/execute.js
+++ JS/execute.js
@@ -1,6 +1,23 @@
+
+
 randomBackground()
 printQuote(getRandomQuote());
 
+setInterval('printQuote(getRandomQuote())', 25000);
+setInterval('randomBackground()', 25000);
+
+// found help with this here: https://www.encodedna.com/javascript/auto-refresh-page-every-10-second-using-javascript-setInterval-method.htm
+// code to refresh the page
+
+// Major takeaway here was that the function needs to be a string inside the setInterval to work which no other documention showed
+
+// setInterval('refresh()', 25000); 
+
+//     function refresh() {
+//         location.reload();
+//     }
+
+ 
 
 // below space for bugfixing and testing
 
