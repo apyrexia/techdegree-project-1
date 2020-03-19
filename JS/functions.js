c303dcb111e1f0080a617bfb6fc6fb47d9ddc23d
Ethan W <elwaterson@gmail.com>
2020-03-18 21:40:47 -0400

    Create getRandomQuote function

--- JS/functions.js
+++ JS/functions.js
@@ -5,9 +5,11 @@ function randomNumber(min, max) {
   return Math.floor( Math.random() * (max - min + 1) + min );
 }
 
-// Code to randomly select a quote from the quotes array and return it.
+// Code to randomly select a quote from the quotes array and return it in HTML.
 function getRandomQuote() {
-
+  selection = randomNumber(1, 12);
+  let message = `<h2>${quotes[selection].quote}</h2> 
+  <h4>-${quotes[selection].source}, ${quotes[selection].citation}, ${quotes[selection].year}</h4>`;
 }
 
 // Code to print to the page
