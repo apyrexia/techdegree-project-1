d1ac6662f7ba09cbde954d69cca761a0455eb837
Ethan W <elwaterson@gmail.com>
2020-03-18 21:20:04 -0400

    Organized project folder

---
+++ JS/functions.js
@@ -0,0 +1,14 @@
+// Random number generator
+function randomNumber(min, max) {
+  return Math.floor( Math.random() * (max - min) + min );
+}
+
+// Code to randomly select a quote from the quotes array and return it.
+function getRandomQuote() {
+
+}
+
+// Code to print to the page
+function printQuote() {
+
+}
