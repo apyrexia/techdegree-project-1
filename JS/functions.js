df8022d81eb400e6a161025f4289d92a7228cfb3
Ethan W <elwaterson@gmail.com>
2020-03-18 21:27:53 -0400

    Created random number generator

--- JS/functions.js
+++ JS/functions.js
@@ -1,6 +1,8 @@
-// Random number generator
+// Random number generator inclusive at max and min source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function randomNumber(min, max) {
-  return Math.floor( Math.random() * (max - min) + min );
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor( Math.random() * (max - min + 1) + min );
 }
 
 // Code to randomly select a quote from the quotes array and return it.
